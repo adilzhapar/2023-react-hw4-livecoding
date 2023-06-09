917d5da79187a51c490745e00fb3690a5f492655
Adil Zhapar <adilzhapar2002@gmail.com>
2023-06-09 15:11:06 +0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-900 text-white mt-8 py-4">
+            <div className="container mx-auto">
+                <p className="text-center">
+                    &copy; Rick&Morty App. Made with love by Adil
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
